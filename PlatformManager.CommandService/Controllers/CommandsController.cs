f272e6d177c3575fa2fbf2d0f1863e2c3272d15f
kyrillosgaber <kyrillosgaber@gmail.com>
2023-08-19 19:01:46 +0300

    create command controller

---
+++ PlatformManager.CommandService/Controllers/CommandsController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformManager.CommandService.Data;
+using PlatformManager.CommandService.Dtos;
+
+namespace PlatformManager.CommandService.Controllers;
+
+[Route("api/c/platforms/{platformId:int}/[controller]")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+    private readonly ICommandRepo _commandRepo;
+    private readonly IMapper _mapper;
+
+    public CommandsController(ICommandRepo commandRepo, IMapper mapper)
+    {
+        _commandRepo = commandRepo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands(int platformId)
+    {
+        Console.WriteLine($"--> getting commands by platform id: {platformId} id from controllers");
+
+        if (!_commandRepo.PlatfromExists(platformId))
+            return NotFound();
+
+        return Ok(_mapper.Map<CommandReadDto>(_commandRepo.GetCommandsForPlatfrom(platformId)));
+    }
+
+    [HttpGet("{commandId:int}", Name = "GetCommandForPlatform")]
+    public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId,int commandId)
+    {
+        Console.WriteLine($"--> getting command: {commandId} by platform id: {platformId} id from controllers");
+
+        if (!_commandRepo.PlatfromExists(platformId))
+            return NotFound();
+        var command = _commandRepo.GetCommand(platformId, commandId);
+        
+        if (command is null)
+            return NotFound();
+
+        return Ok(_mapper.Map<CommandReadDto>(command));
+    }
+}
